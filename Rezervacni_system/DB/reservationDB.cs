b09f78cf0282ae5aa5154d348a24351703867ed2
Bruno Bébr <letaxcz@gmail.com>
2023-05-03 23:07:47 +0200

    goodnight davide (commit v 23:07)

---
+++ Rezervacni_system/DB/reservationDB.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rezervacni_system.DB
+{
+    public class reservationDB
+    {
+         [PrimaryKey, AutoIncrement]
+            public int Id { get; set; }
+            public int Uuid { get; set; }
+            public int row { get; set; }
+            public int column { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string tel { get; set; }
+            public string status { get; set; }
+        
+    }
+}
---
+++ Rezervacni_system/DBHandler.cs
@@ -0,0 +1,27 @@
+﻿using Rezervacni_system.DB;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rezervacni_system
+{
+    public class DBHandler
+    {
+ 
+            private SQLiteConnection _db;
+
+            public DBHandler()
+            {
+
+                _db = new SQLiteConnection("../../DB");
+                _db.CreateTable<reservationDB>();
+               
+            }
+      
+     
+
+    }
+}
